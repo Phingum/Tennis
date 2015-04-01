1f257333572942d9d9f8c334c5184a176401b68d
Simon Brix <simonbrix@pc25.home>
2015-04-01 20:54:42 +0200

    added support for doubles

--- Program.cs
+++ Program.cs
@@ -35,6 +35,47 @@ namespace TennisTournament
 		}
 	}
 
+	public class Team
+	{
+		readonly TennisPlayer player1;
+		readonly TennisPlayer player2;
+
+		public Team(TennisPlayer p1)
+		{
+			player1 = p1;
+		}
+
+		public Team(TennisPlayer p1, TennisPlayer p2)
+		{
+			player1 = p1; player2 = p2;
+		}
+
+		public bool TeamSize()
+		{
+			return player2 != null ? true : false;
+		}
+
+		public string GetFirstName()
+		{
+			return TeamSize() ? String.Format("{0} & {1}", player1.FirstName, player2.FirstName) : String.Format("{0}", player1.FirstName);
+		}
+
+		public string GetFullName()
+		{
+			return TeamSize() ? String.Format("{0} {1} {2} & {3} {4} {5}", player1.FirstName, player1.MiddleName, player1.LastName, player2.FirstName, player2.MiddleName, player2.LastName) : String.Format("{0} {1} {2}", player1.FirstName, player1.MiddleName, player1.LastName);
+		}
+
+		public TennisPlayer ReturnP1()
+		{
+			return player1;
+		}
+
+		public TennisPlayer ReturnP2()
+		{
+			return player2 != null ? player2 : null;
+		}
+	}
+
 	public class Referee : Person
 	{
 		public bool GamesMaster { get; set; }
@@ -55,21 +96,16 @@ namespace TennisTournament
 		public DateTime TStart { get; set; }
 		public DateTime TEnd { get; set; }
 		public int NoPlayers { get; set; }
-		public List<TennisPlayer> _playerList = new List<TennisPlayer>();
-		public List<TennisPlayer> _roundWinners = new List<TennisPlayer>();
-		public List<TennisPlayer> _tempPlayerList = new List<TennisPlayer>();
+		public List<Team> _playerList = new List<Team>();
+		public List<Team> _roundWinners = new List<Team>();
+		public List<Team> _tempPlayerList = new List<Team>();
 		public List<Referee> _tournamentReferees = new List<Referee>();
 		private int NoOfRounds;
-		private int count;
-		private TennisPlayer tournamentWinner;
+		private Team tournamentWinner;
 
-		public Tournament(string n, int y, DateTime from, DateTime to, int np = 8, params TennisPlayer[] args)
+		public Tournament(string n, int y, DateTime from, DateTime to, int np = 8)
 		{
-			Name = n; Year = y; TStart = from; TEnd = to; NoPlayers = np; count = args.Length;
-			for (int i = 0; i < count; i++)
-			{
-				_playerList.Add(args[i]);
-			}
+			Name = n; Year = y; TStart = from; TEnd = to; NoPlayers = np;
 			NoOfRounds = 1;
 			int PlayersLeft = NoPlayers;
 			while (PlayersLeft != 2)
@@ -86,12 +122,12 @@ namespace TennisTournament
 			{
 				Console.WriteLine("ROUND {0}!", j+1);
 				Console.Write("PLAYERS: ");
-				_roundWinners.ForEach(i => Console.Write("{0}, ", i.FirstName));
+				_roundWinners.ForEach(i => Console.Write("{0}, ", i.GetFirstName()));
 				Console.WriteLine();
 				int NoOfMatches = _roundWinners.Count / 2;
 				for (int i = 0; i < _roundWinners.Count; i = i+2)
 				{
-					TennisPlayer winner_player;
+					Team winner_player;
 					var match = new TennisMatch(_playerList[i], _playerList[i+1]);
 					match.RunMatch(noOfSets, out winner_player);
 					_tempPlayerList.Add(winner_player);
@@ -112,7 +148,7 @@ namespace TennisTournament
 
 		public void ReturnPlayerList()
 		{
-			_playerList.ForEach(i => Console.Write("{0}, ", i.FirstName));
+			_playerList.ForEach(i => Console.Write("{0}, ", i.GetFirstName()));
 			Console.WriteLine();
 		}
 
@@ -125,7 +161,7 @@ namespace TennisTournament
 			}
 		}
 
-		public void AddPlayer(TennisPlayer player)
+		public void AddPlayer(Team player)
 		{
 			if(_playerList.Contains(player))
 			{
@@ -137,7 +173,7 @@ namespace TennisTournament
 			}
 		}
 
-		public void RemovePlayer(TennisPlayer player)
+		public void RemovePlayer(Team player)
 		{
 			_playerList.Remove(player);
 		}
@@ -175,7 +211,7 @@ namespace TennisTournament
 
 		public string ReturnWinner()
 		{
-			return string.Format("TOURNAMENT WINNER IS: {0} {1} {2} at the age of {3}", tournamentWinner.FirstName, tournamentWinner.MiddleName, tournamentWinner.LastName, tournamentWinner.GetAge());
+			return string.Format("TOURNAMENT WINNER IS: {0}", tournamentWinner.GetFullName());
 		}
 
 		public override string ToString()
@@ -186,19 +222,19 @@ namespace TennisTournament
 
 	public class TennisMatch 
 	{
-		readonly TennisPlayer player1;
-		readonly TennisPlayer player2;
+		readonly Team team1;
+		readonly Team team2;
 		static Random rand = new Random();
 
-		public TennisMatch(TennisPlayer player1, TennisPlayer player2)
+		public TennisMatch(Team team1, Team team2)
 		{
-			this.player1 = player1; this.player2 = player2;
+			this.team1 = team1; this.team2 = team2;
 		}
 
-		public bool CheckGender()
+		/*public bool CheckGender()
 		{
-			return player1.PersonGender == player2.PersonGender ? true : false;
-		}
+			return team1.PersonGender == team2.PersonGender ? true : false;
+		}*/
 
 		static void SingleSet(out int rand1, out int rand2)
 		{
@@ -218,14 +254,14 @@ namespace TennisTournament
 			}
 		}
 
-		public string RunMatch(int noOfSets, out TennisPlayer winner_player)
+		public string RunMatch(int noOfSets, out Team winner_player)
 		{
 			int rand1;
 			int rand2;
 			int cul_left = 0;
 			int cul_right = 0;
-			winner_player = player1;
-			Console.WriteLine("\n{0} vs {1}", player1.FirstName, player2.FirstName);
+			winner_player = team1;
+			Console.WriteLine("\n{0} vs {1}", team1.GetFirstName(), team2.GetFirstName());
 			for (int j = 0; j < noOfSets; j++)
 			{
 				SingleSet(out rand1, out rand2);
@@ -234,13 +270,13 @@ namespace TennisTournament
 			}
 			if (cul_left > cul_right)
 			{
-				Console.WriteLine(player1.FirstName + " wins! " + cul_left + " - " + cul_right);
-				winner_player = player1;
+				Console.WriteLine(team1.GetFirstName() + " wins! " + cul_left + " - " + cul_right);
+				winner_player = team1;
 			}
 			else
 			{
-				Console.WriteLine(player2.FirstName + " wins! " + cul_right + " - " + cul_left);
-				winner_player = player2;
+				Console.WriteLine(team2.GetFirstName() + " wins! " + cul_right + " - " + cul_left);
+				winner_player = team2;
 			}
 			Console.WriteLine();
 			return "";
@@ -259,27 +295,42 @@ namespace TennisTournament
 			var p6 = new TennisPlayer("Carsten", "Bruun", "Vestergaard", new DateTime(1993, 10, 24), "Dansk", TennisPlayer.Gender.Mand);
 			var p7 = new TennisPlayer("Jakob", "J.", "Jakobsen", new DateTime(1993, 10, 24), "Dansk", TennisPlayer.Gender.Mand);
 			var p8 = new TennisPlayer("Anders", "A.", "Andersen", new DateTime(1993, 10, 24), "Dansk", TennisPlayer.Gender.Mand);
+			var team1 = new Team(p1);
+			var team2 = new Team(p2);
+			var team3 = new Team(p3);
+			var team4 = new Team(p4);
+			var team5 = new Team(p5);
+			var team6 = new Team(p6);
+			var team7 = new Team(p7);
+			var team8 = new Team(p8);
+			var team1d = new Team(p1, p2);
+			var team2d = new Team(p3, p4);
+			var team3d = new Team(p5, p6);
+			var team4d = new Team(p7, p8);
 			var ref1 = new Referee(new DateTime(2001, 08, 24), new DateTime(2014, 12, 24), "Kristoffer", "Mæng", "Nielsen", new DateTime(1991, 04, 01), "Dansk", Referee.Gender.Mand);
 			var ref2 = new Referee(new DateTime(2003, 10, 02), new DateTime(2015, 01, 19), "Niclas", "Allentoft", "Jørgensen", new DateTime(1991, 08, 10), "Dansk", Referee.Gender.Mand);
-			var tourn1 = new Tournament("Wimbledon", 2015, new DateTime(2015, 01, 15), new DateTime(2015, 02, 15), 8, p1, p2, p3, p4, p5, p6, p7, p8);
+			var tourn1 = new Tournament("Wimbledon", 2015, new DateTime(2015, 01, 15), new DateTime(2015, 02, 15), 4);
 			var tourn2 = new Tournament("Wimbledon, I guess", 2015, new DateTime(2015, 01, 15), new DateTime(2015, 02, 15));
-			//tourn1.ReturnPlayerList();
-			//tourn1.PlayTournament(3);
-			/*tourn2.AddPlayer(p1);
-			tourn2.AddPlayer(p2);
-			tourn2.AddPlayer(p3);
-			tourn2.AddPlayer(p4);
-			tourn2.AddPlayer(p5);
-			tourn2.AddPlayer(p6);
-			tourn2.AddPlayer(p7);
-			tourn2.AddPlayer(p8);
+			tourn1.AddPlayer(team1d);
+			tourn1.AddPlayer(team2d);
+			tourn1.AddPlayer(team3d);
+			tourn1.AddPlayer(team4d);
+			tourn1.PlayTournament(3);
+			/*tourn2.AddPlayer(team1);
+			tourn2.AddPlayer(team2);
+			tourn2.AddPlayer(team3);
+			tourn2.AddPlayer(team4);
+			tourn2.AddPlayer(team5);
+			tourn2.AddPlayer(team6);
+			tourn2.AddPlayer(team7);
+			tourn2.AddPlayer(team8);
 			tourn2.PlayTournament(3);*/
-			tourn1.AddReferee(ref1);
+			/*tourn1.AddReferee(ref1);
 			tourn1.AddReferee(ref2);
 			tourn1.ReturnRefereeList();
 			tourn1.AddGameMaster(ref1);
 			tourn1.ReturnRefereeList();
-			/*tourn1.RemoveReferee(ref1);
+			tourn1.RemoveReferee(ref1);
 			tourn1.ReturnRefereeList();*/
 		} 
 	}
